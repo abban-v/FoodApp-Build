c20a49e2b1b5a0b479f8e8a47842e8004561e811
Abban <abban.varughese@gmail.com>
2025-10-04 15:22:14 +0530

    Final attempt: Use official MainApplication.kt template

--- android/app/src/main/java/com/foodapp/MainApplication.kt
+++ android/app/src/main/java/com/foodapp/MainApplication.kt
@@ -3,11 +3,9 @@ package com.foodapp
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
@@ -20,19 +18,16 @@ class MainApplication : Application(), ReactApplication {
 
         override fun getJSMainModuleName(): String = "index"
 
-        override fun getUseDeveloperSupport(): Boolean = true // Hardcoded to true for debug
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-        override val isNewArchitectureEnabled: Boolean = false // Hardcoded to false
-        override val isHermesEnabled: Boolean = true // Hardcoded to true
+        override val isNewArchitectureEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
-    if (isNewArchitectureEnabled) {
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
